52c2f1fa050c463a64ebbe683d1722721a54c36c
remoflury <remoflury97+web@gmail.com>
2023-11-30 21:05:18 +0100

    adjustments tutorial & desktop fallback

--- src/lib/components/overlays/tutorial.svelte
+++ src/lib/components/overlays/tutorial.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import { showTutorial } from '$lib/store/gameStore';
+	import { gameStore, showTutorial } from '$lib/store/gameStore';
 	import { fly } from 'svelte/transition';
 </script>
 
 {#if $showTutorial}
-	<section transition:fly={{ duration: 350, y: 200 }} class="absolute inset-0 bg-white-soft">
+	<section transition:fly={{ duration: 350, y: -200 }} class="absolute inset-0 bg-white-soft">
 		<article class="container py-block-page">
 			<h1>Tutorial</h1>
 			<button on:click={() => ($showTutorial = !$showTutorial)}>Hide Tutorial</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,36 @@
 <script lang="ts">
 	import PrimaryButton from '$lib/components/primaryButton.svelte';
-	import { showTutorial } from '$lib/store/gameStore';
+	import { gameStore, showTutorial } from '$lib/store/gameStore';
+	let nameValue = '';
+
+	const handleStartGame = () => {
+		$showTutorial = true;
+		$gameStore.userName = nameValue;
+	};
 </script>
 
 <svelte:head><title>GameOver? | Start</title></svelte:head>
 
 <section class="bg-gradient-turqoise-to-top min-h-screen py-block-page">
-	<article class="container">
+	<!-- mobile -->
+	<article class="container lg:hidden">
 		<figure class="max-w-1/2 mx-auto">
 			<img src="/assets/gameover-logo-white.svg" alt="Logo von GameOver?" />
 		</figure>
+		<h1 hidden>GameOver?</h1>
 		<p class="text-white-soft text-center mt-8">
 			Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar elementum tempus hac
 			tellus libero accumsan.
 		</p>
-		<PrimaryButton
-			buttonProps={{ text: 'Start Game' }}
-			on:click={() => ($showTutorial = !$showTutorial)}
-		/>
-		<input type="name" placeholder="Name" />
+		<PrimaryButton buttonProps={{ text: 'Start Game' }} on:click={handleStartGame} />
+		<input type="name" placeholder="Name" bind:value={nameValue} />
+	</article>
+
+	<!-- desktop -->
+	<article class="hidden lg:block container">
+		<figure class="max-w-1/2 mx-auto">
+			<img src="/assets/gameover-logo-white.svg" alt="Logo von GameOver?" />
+		</figure>
+		<h1>Bitte öffne diese Seite auf einem mobilen Gerät.</h1>
 	</article>
 </section>
