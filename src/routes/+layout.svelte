0bc64f0b498591b9d99b421e0bc891fd1bfef530
remoflury <remoflury97+web@gmail.com>
2023-12-06 16:20:24 +0100

    removed page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,11 @@
 <script>
 	import '../css/main.css';
-	import { fly } from 'svelte/transition';
-	import { cubicIn } from 'svelte/easing';
-	import { afterNavigate } from '$app/navigation';
-	import { page } from '$app/stores';
 	import Tutorial from '$lib/components/overlays/tutorial.svelte';
-
-	let path = $page.url.pathname;
-	afterNavigate(() => {
-		path = $page.url.pathname;
-	});
 
 	const options = {};
 </script>
 
-{#key path}
-	<main in:fly={{ duration: 200, easing: cubicIn }} class="relative min-h-screen">
-		<slot />
-		<Tutorial />
-	</main>
-{/key}
+<main class="relative min-h-screen">
+	<slot />
+	<Tutorial />
+</main>
