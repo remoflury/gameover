163f2ea6a23f20435c4c46168d6f3410987cda64
remoflury <remoflury97+web@gmail.com>
2023-12-11 15:14:32 +0100

    get leaderboard

--- src/lib/types/Types.ts
+++ src/lib/types/Types.ts
@@ -1,6 +1,7 @@
 export type LeaderboardUsersProps = {
   name: string,
-  score: number
+  score: number,
+  id?: number
 }
 
 export type GameStoreProps = {
@@ -43,4 +44,5 @@ export type ScenarioOptionProps = {
 export type ServerAPIResponseProps = {
   status: 200 | 201 | 400 | 401 | 401 | 402 | 403 | 404 | 500,
   message?: string,
+  data?: any
 }
