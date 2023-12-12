ef14f922ab22b0d02bcbf2bd1941f45228e4b303
remoflury <remoflury97+web@gmail.com>
2023-12-12 10:57:23 +0100

    add category Score

--- src/lib/components/gameScore.svelte
+++ src/lib/components/gameScore.svelte
@@ -1,39 +1,17 @@
 <script lang="ts">
 	import { gameStore } from '$lib/store/gameStore';
-	import { fly, slide } from 'svelte/transition';
+	import { category100PercentValue } from '$lib/utils/generalVariables';
+
+	import EconomyIconMask from '$lib/assets/category/economyIconMask.svelte';
+	import CategoryScoreIcon from './categoryScoreIcon.svelte';
+
+	console.log('economy ', $gameStore.score.economy);
+	console.log('max score ', category100PercentValue);
 </script>
 
-<article class="flex gap-x-2 justify-between container pt-block-page">
-	<div class="flex flex-col gap-y-1 items-center">
-		<p>economy</p>
-		{#key $gameStore.score.economy}
-			<p out:fly={{ y: -10, duration: 250 }} in:fly={{ y: 10, duration: 250, delay: 250 }}>
-				{$gameStore.score.economy}
-			</p>
-		{/key}
-	</div>
-	<div class="flex flex-col gap-y-1 items-center">
-		<p>environment</p>
-		{#key $gameStore.score.environment}
-			<p out:fly={{ y: -10, duration: 250 }} in:fly={{ y: 10, duration: 250, delay: 250 }}>
-				{$gameStore.score.environment}
-			</p>
-		{/key}
-	</div>
-	<div class="flex flex-col gap-y-1 items-center">
-		<p>society</p>
-		{#key $gameStore.score.society}
-			<p out:fly={{ y: -10, duration: 250 }} in:fly={{ y: 10, duration: 250, delay: 250 }}>
-				{$gameStore.score.society}
-			</p>
-		{/key}
-	</div>
-	<div class="flex flex-col gap-y-1 items-center">
-		<p>health</p>
-		{#key $gameStore.score.health}
-			<p out:fly={{ y: -10, duration: 250 }} in:fly={{ y: 10, duration: 250, delay: 250 }}>
-				{$gameStore.score.health}
-			</p>
-		{/key}
-	</div>
+<article class="flex gap-x-2 justify-evenly container pt-block-page">
+	<CategoryScoreIcon category="economy" currentScore={$gameStore.score.economy} />
+	<CategoryScoreIcon category="environment" currentScore={$gameStore.score.environment} />
+	<CategoryScoreIcon category="society" currentScore={$gameStore.score.society} />
+	<CategoryScoreIcon category="health" currentScore={$gameStore.score.health} />
 </article>
--- src/lib/utils/generalVariables.ts
+++ src/lib/utils/generalVariables.ts
@@ -1,1 +1,3 @@
 export const serverErrMessage = 'Ups, da lief etwas schief. Bitte probiere es später nochmal.'
+
+export const category100PercentValue = 100
